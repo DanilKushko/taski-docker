0441ee5e0f42d224a3a3cb4689865942a22d68a1
Данил Кушко <Danilkushko1@gmail.com>
2023-06-05 17:44:11 +0700

    oh my

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -9,12 +9,12 @@ class TaskiAPITestCase(TestCase):
         self.guest_client = Client()
 
     def test_list_exists(self):
-        """Проверка доступности списка задач."""
+        """Task availability check."""
         response = self.guest_client.get('/api/tasks/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_task_creation(self):
-        """Проверка создания задачи."""
+        """Task creation check."""
         data = {'title': 'Test', 'description': 'Test'}
         response = self.guest_client.post('/api/tasks/', data=data)
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
